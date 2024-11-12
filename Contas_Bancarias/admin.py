97c8e4b3ae61dcae32278e82aa9b2e168f5baba7
krtps57 <marcelorochafilho04@gmail.com>
2024-11-12 20:31:05 -0300

    criação do models
    criação do models de conta bancarias é o registro dele no admin

--- Contas_Bancarias/admin.py
+++ Contas_Bancarias/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import Conta_bancaria
 
-# Register your models here.
+admin.site.register(Conta_bancaria)
--- Contas_Bancarias/models.py
+++ Contas_Bancarias/models.py
@@ -1,5 +1,10 @@
 from django.db import models
-
+from client.models import client
 class Conta_bancaria(models.Model):
     Número_conta = models.CharField(max_length=6)
+    Titular = models.ForeignKey(client, on_delete=models.CASCADE)
+    Saldo = models.DecimalField(max_digits=10, decimal_places=2)
+    Data_criaçao = models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+        return self.Número_conta
