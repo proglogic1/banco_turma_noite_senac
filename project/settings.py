dc0570f45e0f6bba4760e01b0daa29a8d0bb8d7f
Marcelo-Rocha00 <>
2024-11-12 20:42:40 -0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -123,3 +123,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'  # Usando o servidor SMTP
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'senaccontabancaria2024@gmail.com'
+EMAIL_HOST_PASSWORD = 'contabancaria2024'
