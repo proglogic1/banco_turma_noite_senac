a08accfd961ef0edec4c9f688cb85e170b5207a0
lvfwk <joaorodrigooliver9@gmail.com>
2024-11-12 19:36:51 -0300

    models de client e endereço
    models feito para client e endereço

--- client/models.py
+++ client/models.py
@@ -2,14 +2,23 @@ from django.db import models
 
 class client(models.Model):
     Nome = models.CharField(max_length=100)
-    Data_Nascimento = models.DateField()
     Telefone = models.IntegerField()
-    CPF = models.IntegerField(max_length=11)
+    CPF = models.CharField(max_length=11)
     email = models.EmailField()
-    CEP = models.IntegerField()
-    Genero = models.CharField()
-    senha_1 = models.CharField()
-    senha_2 = models.CharField()
 
     def __str__(self):
         return self.Nome
+
+#==============================================#
+
+class Endereco(models.Model):
+    CEP = models.CharField(max_length=8)
+    logradouro = models.CharField(max_length=150)
+    cidade = models.CharField(max_length=50)
+    UF = models.CharField(max_length=2)
+    Pais =models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.CEP
+
+#==============================================#
