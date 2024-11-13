2122da27227bc5e5dcb0f9a55773932a4116169f
Carlos Celestino <49007553+carloscelestino1@users.noreply.github.com>
2024-11-13 19:43:43 -0300

    Delete Contas_Bancarias directory

--- Contas_Bancarias/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Conta_bancaria, Conta_Corrente, Conta_Poupanca
-
-admin.site.register(Conta_bancaria)
-admin.site.register(Conta_Corrente)
-admin.site.register(Conta_Poupanca)
