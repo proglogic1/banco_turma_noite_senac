d2fbf2b2878656250a6c6c8842bd3ee95b80ee3e
Carlos Celestino <49007553+carloscelestino1@users.noreply.github.com>
2024-11-13 19:44:02 -0300

    Delete client directory

--- client/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render, redirect, get_list_or_404
-from .models import client
-
-def client_list(request):
-    clients = client.objects.all() #Puxar todos os dados da tabela de clients
-
