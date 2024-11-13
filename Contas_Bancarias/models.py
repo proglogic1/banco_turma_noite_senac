f47c74580d72bd3862e0fd6d8646b25a39913f5b
krtps57 <marcelorochafilho04@gmail.com>
2024-11-12 21:16:04 -0300

    atualização
    criação do models de conta poupança é conta corrente

--- Contas_Bancarias/models.py
+++ Contas_Bancarias/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from client.models import client
+
 class Conta_bancaria(models.Model):
     numero_conta = models.CharField(max_length=6)
     titular = models.ForeignKey(client, on_delete=models.CASCADE)
@@ -7,4 +8,19 @@ class Conta_bancaria(models.Model):
     data_criacao = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
-        return self.numero_conta
+        return  f"Conta {self.numero_conta} - Titular: {self.titular}"
+    
+    
+
+class Conta_Corrente(Conta_bancaria):
+    limite_credito = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
+    
+    def __str__(self):
+        return f"Conta corrente {self.numero_conta} - Titular: {self.titular}"
+    
+    
+class Conta_Poupanca(Conta_bancaria):
+    taxa_juros = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
+    
+    def __str__(self):
+        return f"Conta poupança: {self.numero_conta} - Titular: {self.titular}"
