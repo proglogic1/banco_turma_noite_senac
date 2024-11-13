3954ab336053dc4339cf708390e9b6b8bee9cfa1
krtps57 <marcelorochafilho04@gmail.com>
2024-11-13 19:35:58 -0300

    conta corrente é conta bancaria

--- Contas_Bancarias/admin.py
+++ Contas_Bancarias/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Conta_bancaria
+from .models import Conta_bancaria, Conta_Corrente, Conta_Poupanca
 
 admin.site.register(Conta_bancaria)
+admin.site.register(Conta_Corrente)
+admin.site.register(Conta_Poupanca)
--- Contas_Bancarias/models.py
+++ Contas_Bancarias/models.py
@@ -1,26 +1,3 @@
 from django.db import models
 from client.models import client
 
-class Conta_bancaria(models.Model):
-    numero_conta = models.CharField(max_length=6)
-    titular = models.ForeignKey(client, on_delete=models.CASCADE)
-    saldo = models.DecimalField(max_digits=10, decimal_places=2)
-    data_criacao = models.DateTimeField(auto_now_add=True)
-    
-    def __str__(self):
-        return  f"Conta {self.numero_conta} - Titular: {self.titular}"
-    
-    
-
-class Conta_Corrente(Conta_bancaria):
-    limite_credito = models.DecimalField(max_digits=10, decimal_places=2, default=0.0)
-    
-    def __str__(self):
-        return f"Conta corrente {self.numero_conta} - Titular: {self.titular}"
-    
-    
-class Conta_Poupanca(Conta_bancaria):
-    taxa_juros = models.DecimalField(max_digits=5, decimal_places=2, default=0.0)
-    
-    def __str__(self):
-        return f"Conta poupança: {self.numero_conta} - Titular: {self.titular}"
