5c4fd912711c5356d6751263748a0c79217bd2bb
Marcelo <marcelorochafilho04@gmail.com>
2024-11-25 19:48:59 -0300

    att

--- project/settings.py
+++ project/settings.py
@@ -139,5 +139,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'two_factor:login'
 
-LOGIN_REDIRECT_URL = 'two_factor:profile'
+LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
