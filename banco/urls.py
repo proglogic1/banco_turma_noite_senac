e1f1f7cd59f68c8e63bf8247bb1cfff55ddb5df8
lvfwk <joaorodrigooliver9@gmail.com>
2024-11-27 19:55:32 -0300

    .

--- banco/urls.py
+++ banco/urls.py
@@ -1,26 +1,26 @@
 from django.urls import path,include
-from .views import cadastrar_cliente,listar_clientes_contas,ClienteListCreateView, ContaListCreateView
-from .views import Buscar_Cep, endereco
+from django.contrib.auth import views as auth_views
+from .views import * 
+
 
 urlpatterns = [
-    path('',listar_clientes_contas,name='listar_clientes_contas'),
-    path('cadastrar/', cadastrar_cliente, name='cadastrar_cliente'),
     path('lista/',listar_clientes_contas,name='listar_clientes_contas'),#
     path('cadastro/', cadastrar_cliente , name ='cadastro' ),
-    path('', menu, name='menu'),
+    path('', menu, name='menu'),#
     path('cadastrar_conta/', cadastrar_conta , name ='cadastrar_conta' ),
     path('atualizar_cadastro/<int:id>/', atualizar_cadastro, name='atualizar_cadastro'),
 
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+
     path('api/clientes/', ClienteListCreateView.as_view(), name='api-clientes'),
     path('api/contas/', ContaListCreateView.as_view(), name='api-contas'),
+
     path('endereco/', endereco, name='Endereco'), # Renderiza a página inicial com o formulário
-    
-    
-    
     path('CEP/', Buscar_Cep, name='CEP'),# Rota para buscar o CEP
+
     path('transferencia/', realizar_transferencia, name='realizar_transferencia'),
     path('historico/<int:id_conta>/', historico_transacoes, name='historico_transacoes'),
+    path('saque/<int:conta_id>/', realizar_saque, name='realizar_saque'),
+    path('deposito/<int:conta_id>/', realizar_deposito, name='realizar_deposito'),
 ]
-
