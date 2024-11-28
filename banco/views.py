32f48ff3db196875aad68c6964d196dce91f143b
MarioSouzza <ruanvitoraquinooliveira@gmail.com>
2024-11-28 19:12:52 -0300

    att

--- banco/views.py
+++ banco/views.py
@@ -11,7 +11,6 @@ from rest_framework import generics,response,status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-import requests  # type: ignore
 from datetime import time
 from django.contrib import messages
 
