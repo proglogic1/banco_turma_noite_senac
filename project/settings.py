0ed438bfc7cbcab5f2d7ef3480865d3b17e0988d
Marcelo <marcelorochafilho04@gmail.com>
2024-11-28 18:54:51 -0300

    att

--- project/settings.py
+++ project/settings.py
@@ -158,7 +158,7 @@ AUTH_USER_MODEL = 'banco.Cliente'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL = 'two_factor:login'
+LOGIN_URL = 'login'
 
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
@@ -170,5 +170,9 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'marcelorochafilho04@Gmail.com'
-EMAIL_HOST_PASSWORD = 'ulhd yuew frvi xbke'
+EMAIL_HOST_USER = 'senaccontabancaria2024@gmail.com'
+<<<<<<< Updated upstream
+EMAIL_HOST_PASSWORD = 'sqzt zxwk oexk qbcb'
+=======
+EMAIL_HOST_PASSWORD = 'contabancaria2024'
+>>>>>>> Stashed changes
