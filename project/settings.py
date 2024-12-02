d03bbd222c667920cbb2c59fd4fa9b722f39756c
carloscelestino1 <proglogic1@gmail.com>
2024-12-02 20:10:41 -0300

    att
    att

--- project/settings.py
+++ project/settings.py
@@ -125,10 +125,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR, 'STATIC']
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  
+]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 
 
 AUTH_USER_MODEL = 'banco.Cliente'
